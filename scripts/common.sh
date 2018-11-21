e12aac345f2ad7500f3488a8ea009d934aefd989
ldsung <ldsung@udptechnology.com>
2018-11-21 09:42:29 +0000

    Modified deploy script

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,6 +2,7 @@
 
 PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin
 APP_NAME="manager"
+APP_MSG_PREFIX="DL Manager"
 APP_DIR="/var/${APP_NAME}"
 # shellcheck disable=SC2034
 PM2_CONF_PATH="${APP_DIR}/pm2/process.json"
@@ -16,34 +17,36 @@ SYSLOG_PREFIX="[CHEKT]"
 eval "$(wget -q -O - http://169.254.169.254/latest/user-data)"
 
 message() {
-	logger "${SYSLOG_PREFIX} ${NODE_ENV} $1"
+  logger "${SYSLOG_PREFIX} ${NODE_ENV} $1"
 }
 
 slack_message() {
-	local conf_path=${APP_CONF_PATH}
+  local conf_path=${APP_CONF_PATH}
 
-	[ ! -z "${2}" ] && conf_path=${2}
+  [ ! -z "${2}" ] && conf_path=${2}
 
-	if [ ! -f "${conf_path}" ]; then
-		message "salck config file not found"
-		return
-	fi
+  if [ ! -f "${conf_path}" ]; then
+    message "salck config file not found"
+    return
+  fi
 
-	local url
-	local sendTime
-	local timestamp
+  local url
+  local sendTime
+  local timestamp
+  local notiMsg
 
   url=$(jq -r .notification.slack.webhookUrl "${conf_path}")
-	sendTime=$(date +%Y-%m-%dT%H:%M:%S%:z)
-	timestamp=$(date +%s)
+  sendTime=$(date +%Y-%m-%dT%H:%M:%S%:z)
+  timestamp=$(date +%s)
+  notiMsg="${APP_MSG_PREFIX} ${1}"
 
-	local project="{\"title\":\"Project\",\"value\":\"CHeKT\",\"short\":true}"
-	local env="{\"title\":\"Environment\",\"value\":\"${NODE_ENV}\",\"short\":true}"
-	local hostname="{\"title\":\"Hostname\",\"value\":\"${HOSTNAME}\",\"short\":true}"
-	local starttime="{\"title\":\"StartTime\",\"value\":\"${sendTime}\",\"short\":true}"
-	local fields="[${project},${env},${hostname},${starttime}]"
-	local payload="{\"text\": \"$1\",\"color\":\"#FF8000\",\"ts\":\"${timestamp}\",\"fields\":${fields}}"
-	local body="{\"attachments\": [${payload}]}"
+  local project="{\"title\":\"Project\",\"value\":\"CHeKT\",\"short\":true}"
+  local env="{\"title\":\"Environment\",\"value\":\"${NODE_ENV}\",\"short\":true}"
+  local hostname="{\"title\":\"Hostname\",\"value\":\"${HOSTNAME}\",\"short\":true}"
+  local starttime="{\"title\":\"StartTime\",\"value\":\"${sendTime}\",\"short\":true}"
+  local fields="[${project},${env},${hostname},${starttime}]"
+  local payload="{\"text\": \"${notiMsg}\",\"color\":\"#FF8000\",\"ts\":\"${timestamp}\",\"fields\":${fields}}"
+  local body="{\"attachments\": [${payload}]}"
 
-	curl -s -d "${body}" "${url}" > /dev/null 2>&1
+  curl -s -d "${body}" "${url}" > /dev/null 2>&1
 }
